209f8a19dd9eb7ba03ff1763fb92cc170515b20d
Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>
2023-03-07 16:22:25 -0500

    fix spacing
    Signed-off-by: Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>

--- Lab 2/main.py
+++ Lab 2/main.py
@@ -177,28 +177,28 @@ class Client:
         print("Client object created!")
 
         self.encryption_key_dict = {
-            "1803933":	"M7E8erO15CIh902P8DQsHxKbOADTgEPGHdiY0MplTuY=",
-            "1884159":	"PWMKkdXW4VJ3pXBpr9UwjefmlIxYwPzk11Aw9TQ2wZQ=",
-            "1853847":	"UVpoR9emIZDrpQ6pCLYopzE2Qm8bCrVyGEzdOOo2wXw=",
-            "1810192":	"bHdhydsHzwKdb0RF4wG72yGm2a2L-CNzDl7vaWOu9KA=",
-            "1891352":	"iHsXoe_5Fle-PHGtgZUCs5ariPZT-LNCUYpixMC3NxI=",
-            "1811313":	"IR_IQPnIM1TI8h4USnBLuUtC72cQ-u4Fwvlu3q5npA0=",
-            "1804841":	"kE8FpmTv8d8sRPIswQjCMaqunLUGoRNW6OrYU9JWZ4w=",
-            "1881925":	"_B__AgO34W7urog-thBu7mRKj3AY46D8L26yedUwf0I=",
-            "1877711":	"dLOM7DyrEnUsW-Q7OM6LXxZsbCFhjmyhsVT3P7oADqk=",
-            "1830894":	"aM4bOtearz2GpURUxYKW23t_DlljFLzbfgWS-IRMB3U=",
-            "1855191":	"-IieSn1zKJ8P3XOjyAlRcD2KbeFl_BnQjHyCE7-356w=",
-            "1821012":	"Lt5wWqTM1q9gNAgME4T5-5oVptAstg9llB4A_iNAYMY=",
-            "1844339":	"M6glRgMP5Y8CZIs-MbyFvev5VKW-zbWyUMMt44QCzG4=",
-            "1898468":	"SS0XtthxP64E-z4oB1IsdrzJwu1PUq6hgFqP_u435AA=",
-            "1883633":	"0L_o75AEsOay_ggDJtOFWkgRpvFvM0snlDm9gep786I=",
-            "1808742":	"9BXraBysqT7QZLBjegET0e52WklQ7BBYWXvv8xpbvr8=",
-            "1863450":	"M0PgiJutAM_L9jvyfrGDWnbfJOXmhYt_skL0S88ngkU=",
-            "1830190":	"v-5GfMaI2ozfmef5BNO5hI-fEGwtKjuI1XcuTDh-wsg=",
-            "1835544":	"LI14DbKGBfJExlwLodr6fkV4Pv4eABWkEhzArPbPSR8=",
-            "1820930":	"zoTviAO0EACFC4rFereJuc0A-99Xf_uOdq3GiqUpoeU="
+            "1803933": "M7E8erO15CIh902P8DQsHxKbOADTgEPGHdiY0MplTuY=",
+            "1884159": "PWMKkdXW4VJ3pXBpr9UwjefmlIxYwPzk11Aw9TQ2wZQ=",
+            "1853847": "UVpoR9emIZDrpQ6pCLYopzE2Qm8bCrVyGEzdOOo2wXw=",
+            "1810192": "bHdhydsHzwKdb0RF4wG72yGm2a2L-CNzDl7vaWOu9KA=",
+            "1891352": "iHsXoe_5Fle-PHGtgZUCs5ariPZT-LNCUYpixMC3NxI=",
+            "1811313": "IR_IQPnIM1TI8h4USnBLuUtC72cQ-u4Fwvlu3q5npA0=",
+            "1804841": "kE8FpmTv8d8sRPIswQjCMaqunLUGoRNW6OrYU9JWZ4w=",
+            "1881925": "_B__AgO34W7urog-thBu7mRKj3AY46D8L26yedUwf0I=",
+            "1877711": "dLOM7DyrEnUsW-Q7OM6LXxZsbCFhjmyhsVT3P7oADqk=",
+            "1830894": "aM4bOtearz2GpURUxYKW23t_DlljFLzbfgWS-IRMB3U=",
+            "1855191": "-IieSn1zKJ8P3XOjyAlRcD2KbeFl_BnQjHyCE7-356w=",
+            "1821012": "Lt5wWqTM1q9gNAgME4T5-5oVptAstg9llB4A_iNAYMY=",
+            "1844339": "M6glRgMP5Y8CZIs-MbyFvev5VKW-zbWyUMMt44QCzG4=",
+            "1898468": "SS0XtthxP64E-z4oB1IsdrzJwu1PUq6hgFqP_u435AA=",
+            "1883633": "0L_o75AEsOay_ggDJtOFWkgRpvFvM0snlDm9gep786I=",
+            "1808742": "9BXraBysqT7QZLBjegET0e52WklQ7BBYWXvv8xpbvr8=",
+            "1863450": "M0PgiJutAM_L9jvyfrGDWnbfJOXmhYt_skL0S88ngkU=",
+            "1830190": "v-5GfMaI2ozfmef5BNO5hI-fEGwtKjuI1XcuTDh-wsg=",
+            "1835544": "LI14DbKGBfJExlwLodr6fkV4Pv4eABWkEhzArPbPSR8=",
+            "1820930": "zoTviAO0EACFC4rFereJuc0A-99Xf_uOdq3GiqUpoeU="
         }
-        
+
         self.get_console_input()
 
     def get_console_input(self):
