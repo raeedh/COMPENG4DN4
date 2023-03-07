c7520c971be7376f56219dab58d6ea323b3ba365
Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>
2023-03-07 15:30:47 -0500

    use the correct socket to send on server
    Signed-off-by: Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>

--- Lab 2/main.py
+++ Lab 2/main.py
@@ -1,14 +1,19 @@
 import argparse
 import csv
 import socket
+import sys
 from typing import Dict
 
 from cryptography.fernet import Fernet
 
-fernet = None
+HOSTNAME = "0.0.0.0"
 PORT = 50007
 MSG_ENCODING = "utf-8"
 RECV_BUFFER_SIZE = 1024
+MAX_CONNECTION_BACKLOG = 1
+SOCKET_ADDRESS = (HOSTNAME, PORT)
+
+fernet = None
 
 
 class Server:
@@ -18,7 +23,8 @@ class Server:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.process_csv_file()
-        self.start_listening()
+        self.create_listen_socket()
+        self.process_connections_forever()
 
     def process_csv_file(self):
         with open('course_grades_2023.csv', 'r') as csvfile:
@@ -30,28 +36,65 @@ class Server:
                 print(row)
                 self.student_dict[row["ID Number"]] = row
 
-    def start_listening(self):
-        self.socket.bind(('', PORT))
-        self.socket.listen(1)
-        print(f"Listening for connections on port {PORT}")
+    def create_listen_socket(self):
+        try:
+            # Create an IPv4 TCP socket.
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        while True:
-            conn, addr = self.socket.accept()
+            # Set socket layer socket options. This allows us to reuse the socket without waiting for any timeouts.
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-            with conn:
-                print(f"Connection received from {addr[0]} on port {addr[1]}.")
-                while True:
+            # Bind socket to socket address, i.e., IP address and port.
+            self.socket.bind(SOCKET_ADDRESS)
+
+            # Set socket to listen state.
+            self.socket.listen(MAX_CONNECTION_BACKLOG)
+            print(f"Listening for connections on port {PORT}")
+        except Exception as msg:
+            print(msg)
+            sys.exit(1)
+
+    def process_connections_forever(self):
+        try:
+            while True:
+                # Block while waiting for accepting incoming connections. When one is accepted, pass the new (cloned) socket reference to
+                # the connection handler function.
+                self.connection_handler(self.socket.accept())
+        except Exception as msg:
+            print(msg)
+        finally:
+            self.socket.close()
+            sys.exit(1)
+
+    def connection_handler(self, client):
+        conn, addr = client
+        print(f"Connection received from {addr[0]} on port {addr[1]}.")
+
+        with conn:
+            while True:
+                try:
+                    # Receive bytes over the TCP connection. This will block until "at least 1 byte or more" is available.
                     data: bytes = conn.recv(RECV_BUFFER_SIZE)
 
+                    # If recv returns with zero bytes, the other end of the TCP connection has closed (The other end is probably in FIN
+                    # WAIT 2 and we are in CLOSE WAIT.). If so, close the server end of the connection and get the next client connection.
                     if not data:
+                        print("Closing client connection ... ")
                         break
 
                     result = self.parse_data(data)
 
                     if not result:
+                        print("Closing client connection ... ")
                         break
 
-    def parse_data(self, data: bytes) -> bool:
+                    conn.sendall(result)
+                except:
+                    print()
+                    print("Closing client connection ... ")
+                    break
+
+    def parse_data(self, data: bytes) -> bytes:
         match data.decode(MSG_ENCODING).split():
             case student_id, cmd:
                 global fernet
@@ -62,7 +105,7 @@ class Server:
                 if not student:
                     fernet = None
                     print("User not found.")
-                    return False
+                    return b""
 
                 print("User found.")
                 # Re-assign the fernet encryption object based on the student id
@@ -71,25 +114,20 @@ class Server:
                 return self.parse_cmd(cmd, student)
             case _:
                 print("The provided data is improperly formatted!")
-                return False
+                return b""
 
-    def parse_cmd(self, cmd: str, student: Dict) -> bool:
+    def parse_cmd(self, cmd: str, student: Dict) -> bytes:
         match cmd:
             case "GMA":
-                self.return_average("Midterm")
-                return True
+                return self.return_average("Midterm")
             case "GL1A":
-                self.return_average("Lab 1")
-                return True
+                return self.return_average("Lab 1")
             case "GL2A":
-                self.return_average("Lab 2")
-                return True
+                return self.return_average("Lab 2")
             case "GL3A":
-                self.return_average("Lab 3")
-                return True
+                return self.return_average("Lab 3")
             case "GL4A":
-                self.return_average("Lab 4")
-                return True
+                return self.return_average("Lab 4")
             case "GEA":
                 values = self.student_dict.values()
 
@@ -104,29 +142,28 @@ class Server:
                 ret_msg = f"Exam 1 Average: {exam1_sum / len(values)}, Exam 2 Average: {exam2_sum / len(values)}, Exam 3 Average: " \
                           f"{exam3_sum / len(values)}, Exam 4 Average: {exam4_sum / len(values)}"
 
-                self.send_message(ret_msg)
-                return True
+                return self.send_message(ret_msg)
             case "GG":
                 ret_msg = f"Lab 1: {int(student.get('Lab 1'))}, Lab 2: {int(student.get('Lab 2'))}, Lab 3: {int(student.get('Lab 3'))}, " \
                           f"Lab 4: {int(student.get('Lab 4'))}, Midterm: {int(student.get('Midterm'))}, Exam 1: " \
                           f"{int(student.get('Exam 1'))}, Exam 2: {int(student.get('Exam 2'))}, Exam 3: {int(student.get('Exam 3'))}, " \
                           f"Exam 4: {int(student.get('Exam 4'))}"
 
-                self.send_message(ret_msg)
-                return True
+                return self.send_message(ret_msg)
             case _:
                 print("Invalid command!")
-                return False
+                return b""
 
     def return_average(self, key: str):
         values = self.student_dict.values()
         average = sum([int(value[key]) for value in values]) / len(values)
-        self.send_message(f"{key} Average: {average}")
+        return self.send_message(f"{key} Average: {average}")
 
     def send_message(self, message: str):
+        global fernet
         encrypted_message_bytes = fernet.encrypt(message.encode(MSG_ENCODING))
         print("encrypted_message_bytes = ", encrypted_message_bytes)
-        self.socket.sendall(encrypted_message_bytes)
+        return encrypted_message_bytes
 
 
 class Client:
@@ -142,9 +179,10 @@ class Client:
     def get_console_input(self):
         while True:
             self.user_input = input("Enter the student ID number, followed by an applicable command (e.g. 1234567 GMA): ")
+
             try:
                 student_id, cmd = self.user_input.split()
-            except Exception as msg:
+            except Exception:
                 print("The input is invalid, please try again.")
                 continue
 
@@ -179,7 +217,7 @@ class Client:
 
         # Connect to the server using its socket address tuple.
         self.socket.connect((Client.SERVER_HOSTNAME, PORT))
-        print("Connected to \"{}\" on port {}".format(Client.SERVER_HOSTNAME, PORT))
+        print(f"Connected to {Client.SERVER_HOSTNAME} on port {PORT}")
         print()
 
     def connection_send(self):
@@ -201,6 +239,7 @@ class Client:
                 print("Did not receive message from server, closing server connection ... ")
                 self.socket.close()
             else:
+                global fernet
                 decrypted_message_bytes = fernet.decrypt(recvd_bytes)
                 decrypted_message = decrypted_message_bytes.decode(MSG_ENCODING)
                 print("decrypted_message = ", decrypted_message)
