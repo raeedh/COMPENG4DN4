0c943d12054266b1800ce4fcd6be65c3c8f25f42
Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>
2023-03-07 14:40:36 -0500

    done csv parsing, command parsing
    Signed-off-by: Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>

--- Lab 2/main.py
+++ Lab 2/main.py
@@ -1,6 +1,7 @@
 import argparse
 import csv
 import socket
+from socket import socket
 from typing import Dict
 
 from cryptography.fernet import Fernet
@@ -14,28 +15,23 @@ RECV_BUFFER_SIZE = 1024
 class Server:
     def __init__(self):
         print("Server object created!")
-        self.student_dict = None
-        self.socket = None
+        self.student_dict = {}
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.process_csv_file()
-        self.start_listening()
+        # self.start_listening()
 
     def process_csv_file(self):
         with open('course_grades_2023.csv', 'r') as csvfile:
-            reader = csv.reader(csvfile)
+            reader = csv.DictReader(csvfile)
 
             print("Data read from CSV file: ")
 
-            rows = []
-
             for row in reader:
-                rows.append(row)
                 print(row)
-
-        self.student_dict = {}
+                self.student_dict[row["ID Number"]] = row
 
     def start_listening(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(('', PORT))
         self.socket.listen(1)
         print(f"Listening for connections on port {PORT}")
@@ -44,6 +40,7 @@ class Server:
             conn, addr = self.socket.accept()
 
             with conn:
+                print(f"Connection received from {addr[0]} on port {addr[1]}.")
                 while True:
                     data: bytes = conn.recv(RECV_BUFFER_SIZE)
 
@@ -61,6 +58,8 @@ class Server:
                 global fernet
                 student: Dict = self.student_dict.get(student_id)
 
+                print(f"Received {cmd} command from client.")
+
                 if not student:
                     fernet = None
                     print("User not found.")
@@ -68,35 +67,65 @@ class Server:
 
                 print("User found.")
                 # Re-assign the fernet encryption object based on the student id
-                fernet = Fernet(student.get("key").encode(MSG_ENCODING))
+                fernet = Fernet(student.get("Key").encode(MSG_ENCODING))
 
-                return self.parse_cmd(cmd)
+                return self.parse_cmd(cmd, student)
             case _:
                 print("The provided data is improperly formatted!")
                 return False
 
-    def parse_cmd(self, cmd: bytes) -> bool:
+    def parse_cmd(self, cmd: bytes, student: Dict) -> bool:
         match cmd:
             case b"GMA":
+                self.return_average("Midterm")
                 return True
             case b"GL1A":
+                self.return_average("Lab 1")
                 return True
             case b"GL2A":
+                self.return_average("Lab 2")
                 return True
             case b"GL3A":
+                self.return_average("Lab 3")
                 return True
             case b"GL4A":
+                self.return_average("Lab 4")
                 return True
             case b"GEA":
+                values = self.student_dict.values()
+
+                exam1_sum, exam2_sum, exam3_sum, exam4_sum = 0, 0, 0, 0
+
+                for value in values:
+                    exam1_sum += value["Exam 1"]
+                    exam2_sum += value["Exam 2"]
+                    exam3_sum += value["Exam 3"]
+                    exam4_sum += value["Exam 4"]
+
+                ret_msg = f"Exam 1 Average: {exam1_sum / len(values)}, Exam 2 Average: {exam2_sum / len(values)}, Exam 3 Average: " \
+                          f"{exam3_sum / len(values)}, Exam 4 Average: {exam4_sum / len(values)}"
+
+                self.send_message(ret_msg)
                 return True
             case b"GG":
+                ret_msg = f"Lab 1: {student.get('Lab 1')}, Lab 2: {student.get('Lab 2')}, Lab 3: {student.get('Lab 3')}, Lab 4: " \
+                          f"{student.get('Lab 4')}, Midterm: {student.get('Midterm')}, Exam 1: {student.get('Exam 1')}, Exam 2: " \
+                          f"{student.get('Exam 2')}, Exam 3: {student.get('Exam 3')}, Exam 4: {student.get('Exam 4')}"
+
+                self.send_message(ret_msg)
                 return True
             case _:
                 print("Invalid command!")
                 return False
 
+    def return_average(self, key: str):
+        values = self.student_dict.values()
+        average = sum([value[key] for value in values]) / len(values)
+        self.send_message(f"{key} Average: {average}")
+
     def send_message(self, message: str):
         encrypted_message_bytes = fernet.encrypt(message.encode(MSG_ENCODING))
+        print("encrypted_message_bytes = ", encrypted_message_bytes)
         self.socket.sendall(encrypted_message_bytes)
 
 
