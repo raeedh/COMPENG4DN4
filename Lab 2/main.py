588f5dc9276b065edf2d0c0148936c529931d08f
Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>
2023-03-07 13:32:45 -0500

    update server
    Signed-off-by: Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>

--- Lab 2/main.py
+++ Lab 2/main.py
@@ -1,38 +1,108 @@
 import argparse
 import socket
+from typing import Dict
 
 from cryptography.fernet import Fernet
 import csv
 
-fernet = Fernet(Fernet.generate_key())
+fernet = None
 PORT = 50007
+
 
 class Server:
     def __init__(self):
         print("Server object created!")
+        self.student_dict = None
+        self.socket = None
 
+        self.process_csv_file()
+        self.start_listening()
+
+    def process_csv_file(self):
         with open('course_grades_2023.csv', 'r') as csvfile:
             reader = csv.reader(csvfile)
 
             print("Data read from CSV file: ")
 
+            rows = []
+
             for row in reader:
+                rows.append(row)
                 print(row)
 
-        # self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self.socket.bind(('', PORT))
-        # self.socket.listen(1)
+        self.student_dict = {}
+
+    def start_listening(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(('', PORT))
+        self.socket.listen(1)
         print(f"Listening for connections on port {PORT}")
+
+        while True:
+            conn, addr = self.socket.accept()
+
+            with conn:
+                while True:
+                    data: bytes = conn.recv(1024)
+
+                    if not data:
+                        break
+
+                    result = self.parse_data(data)
+
+                    if not result:
+                        break
+
+    def parse_data(self, data: bytes) -> bool:
+        match data.split():
+            case student_id, cmd:
+                global fernet
+                student: Dict = self.student_dict.get(student_id)
+
+                if not student:
+                    fernet = None
+                    print("User not found.")
+                    return False
+
+                print("User found.")
+                # Re-assign the fernet encryption object based on the student id
+                fernet = Fernet(student.get("key").encode('utf-8'))
+
+                return self.parse_cmd(cmd)
+            case _:
+                print("The provided data is improperly formatted!")
+                return False
+
+    def parse_cmd(self, cmd: bytes) -> bool:
+        match cmd:
+            case b"GMA":
+                return True
+            case b"GL1A":
+                return True
+            case b"GL2A":
+                return True
+            case b"GL3A":
+                return True
+            case b"GL4A":
+                return True
+            case b"GEA":
+                return True
+            case b"GG":
+                return True
+            case _:
+                print("Invalid command!")
+                return False
 
     def send_message(self, message: str):
         encrypted_message_bytes = fernet.encrypt(message.encode('utf-8'))
+        self.socket.sendall(encrypted_message_bytes)
 
 
 class Client:
     def __init__(self):
-        print("We have created a Client object: ", self)
-        pass
+        print("Client object created!")
 
+        self.id = input("Please enter your student ID: ")
 
     def run_command(self):
         # send command to server via socket
