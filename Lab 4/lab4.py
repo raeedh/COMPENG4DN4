ed93903d6289417e5d0139ec3dcd48d4ca859835
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2023-04-02 20:45:16 -0400

    client done (maybe?)

--- Lab 4/lab4.py
+++ Lab 4/lab4.py
@@ -8,6 +8,9 @@ import socket
 import sys
 from threading import Thread
 import ipaddress
+import select
+import fcntl
+import os
 
 ########################################################################
 
@@ -41,7 +44,8 @@ FILESIZE_FIELD_LEN = 8  # 8 byte file size field.
 CMD = {
     "getdir": 1,
     "makeroom": 2,
-    "deleteroom": 3
+    "deleteroom": 3,
+    "chat": 4
 }
 
 MSG_ENCODING = "utf-8"
@@ -361,6 +365,9 @@ class Client:
     RECV_SIZE = 256
 
     def __init__(self):
+        # Default chat name: user
+        self.chat_name = "user: "
+
         self.get_console_input()
 
     def get_console_input(self):
@@ -372,7 +379,7 @@ class Client:
                     self.connect_to_server()
                 except Exception as msg:
                     print(msg)
-                    print("Connection to CRDS, please try again.\n")
+                    print("Connection to CRDS failed, please try again.\n")
                     continue
 
                 try:
@@ -397,8 +404,6 @@ class Client:
                 except (KeyboardInterrupt, EOFError):
                     print("Closing chat connection...\n")
                     continue
-
-            print("The input is invalid, please try again.\n")
 
     def connect_to_server(self):
         # Create an IPv4 TCP socket.
@@ -518,7 +523,7 @@ class Client:
 
     def deleteroom(self, chat_room_name):
         # Create the packet cmd field.
-        cmd_field = CMD["makeroom"].to_bytes(CMD_FIELD_LEN, byteorder='big')
+        cmd_field = CMD["deleteroom"].to_bytes(CMD_FIELD_LEN, byteorder='big')
 
         chat_room_name_bytes = chat_room_name.encode(MSG_ENCODING)
 
@@ -530,68 +535,169 @@ class Client:
         # Send the request packet to the server.
         self.socket.sendall(pkt)
 
+    def getchat(self, chat_room_name):
+        # Create the packet cmd field.
+        cmd_field = CMD["chat"].to_bytes(CMD_FIELD_LEN, byteorder='big')
+
+        chat_room_name_bytes = chat_room_name.encode(MSG_ENCODING)
+
+        chat_room_name_size_field = len(chat_room_name_bytes).to_bytes(FILENAME_SIZE_FIELD_LEN, byteorder='big')
+
+        pkt = cmd_field \
+            + chat_room_name_size_field + chat_room_name_bytes
+
+        # Send the request packet to the server.
+        self.socket.sendall(pkt)
+
+        ################################################################
+        # Process the chat room response from the server
+
+        # Read the chat room address size field returned by the server.
+        status, chat_room_address_size_field = recv_bytes(self.socket, FILENAME_SIZE_FIELD_LEN)
+        if not status:
+            print("Closing connection ...\n")
+            self.socket.close()
+            return
+
+        chat_room_address_size_bytes = int.from_bytes(chat_room_address_size_field, byteorder='big')
+        if not chat_room_address_size_bytes:
+            print("Connection is closed!\n")
+            self.socket.close()
+            return
+
+        # Now read and decode the requested chat room address.
+        status, chat_room_address_bytes = recv_bytes(self.socket, chat_room_address_size_bytes)
+        if not status:
+            print("Closing connection ...")
+            self.socket.close()
+            return
+        if not chat_room_address_bytes:
+            print("Connection is closed!")
+            self.socket.close()
+            return
+
+        chat_room_address = chat_room_address_bytes.decode(MSG_ENCODING)
+
+        # Read the chat room port size field returned by the server.
+        status, chat_room_port_size_field = recv_bytes(self.socket, FILENAME_SIZE_FIELD_LEN)
+        if not status:
+            print("Closing connection ...\n")
+            self.socket.close()
+            return
+
+        chat_room_port_size_bytes = int.from_bytes(chat_room_port_size_field, byteorder='big')
+        if not chat_room_port_size_bytes:
+            print("Connection is closed!\n")
+            self.socket.close()
+            return
+
+        # Now read and decode the requested chat room address.
+        status, chat_room_port_bytes = recv_bytes(self.socket, chat_room_port_size_bytes)
+        if not status:
+            print("Closing connection ...")
+            self.socket.close()
+            return
+        if not chat_room_port_bytes:
+            print("Connection is closed!")
+            self.socket.close()
+            return
+
+        chat_room_port = chat_room_port_bytes.decode(MSG_ENCODING)
+
+        self.chat_address = chat_room_address
+        self.chat_port = chat_room_port
+
     def chatroom(self, name):
-        self.get_socket()
-        self.receive_forever()
-
-    def get_socket(self):
         try:
-            self.chat_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.chat_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-
-            ############################################################            
-            # Bind to an address/port. In multicast, this is viewed as
-            # a "filter" that deterimines what packets make it to the
-            # UDP app.
-            ############################################################            
-            self.chat_socket.bind(RX_BIND_ADDRESS_PORT)
-
-            ############################################################
-            # The multicast_request must contain a bytes object
-            # consisting of 8 bytes. The first 4 bytes are the
-            # multicast group address. The second 4 bytes are the
-            # interface address to be used. An all zeros I/F address
-            # means all network interfaces. They must be in network
-            # byte order.
-            ############################################################
-            multicast_group_bytes = socket.inet_aton(MULTICAST_ADDRESS)
-            # or
-            # multicast_group_int = int(ipaddress.IPv4Address(MULTICAST_ADDRESS))
-            # multicast_group_bytes = multicast_group_int.to_bytes(4, byteorder='big')
-            # or
-            # multicast_group_bytes = ipaddress.IPv4Address(MULTICAST_ADDRESS).packed
-            print("Multicast Group: ", MULTICAST_ADDRESS)
-
-            # Set up the interface to be used.
-            multicast_iface_bytes = socket.inet_aton(RX_IFACE_ADDRESS)
-
-            # Form the multicast request.
-            multicast_request = multicast_group_bytes + multicast_iface_bytes
-            print("multicast_request = ", multicast_request)
-
-            # You can use struct.pack to create the request, but it is more complicated, e.g.,
-            # 'struct.pack("<4sl", multicast_group_bytes,
-            # int.from_bytes(multicast_iface_bytes, byteorder='little'))'
-            # or 'struct.pack("<4sl", multicast_group_bytes, socket.INADDR_ANY)'
-
-            # Issue the Multicast IP Add Membership request.
-            print("Adding membership (address/interface): ", MULTICAST_ADDRESS,"/", RX_IFACE_ADDRESS)
-            self.chat_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, multicast_request)
+            self.connect_to_server()
         except Exception as msg:
             print(msg)
-            sys.exit(1)
+            print("Unable to retrieve multicast address/port for the associated chat room, please try again.\n")
+            return
+
+        try:
+            self.getchat(name)
+        except Exception as msg:
+            print(msg)
+            print("Unable to retrieve multicast address/port for the associated chat room, please try again.\n")
+            self.socket.close()
+            return
+
+        try:
+            self.get_socket()
+        except Exception as msg:
+            print(msg)
+            print("Error connecting to chat room, please try again.")
+            return
+        try:
+            self.receive_forever()
+        except Exception as msg:
+            print(msg)
+            print("Error connecting to chat room, please try again.")
+            self.chat_socket.close()
+            return
+
+    def get_socket(self):
+        self.chat_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.chat_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        
+        self.chat_socket.bind((RX_BIND_ADDRESS, self.chat_port))
+
+        multicast_group_bytes = socket.inet_aton(self.chat_address)
+        # print("Multicast Group: ", MULTICAST_ADDRESS)
+
+        # Set up the interface to be used.
+        multicast_iface_bytes = socket.inet_aton(RX_IFACE_ADDRESS)
+
+        # Form the multicast request.
+        multicast_request = multicast_group_bytes + multicast_iface_bytes
+        # print("multicast_request = ", multicast_request)
+
+        # Issue the Multicast IP Add Membership request.
+        # print("Adding membership (address/interface): ", MULTICAST_ADDRESS,"/", RX_IFACE_ADDRESS)
+        self.chat_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, multicast_request)
 
     def receive_forever(self):
+        # Set stdin to non-blocking mode
+        fd = sys.stdin.fileno()
+        flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+
         while True:
-            try:
-                data, address_port = self.chat_socket.recvfrom(Client.RECV_SIZE)
-                address, port = address_port
-                print("Received: {} {}".format(data.decode('utf-8'), address_port))
-            except KeyboardInterrupt:
-                print(); exit()
-            except Exception as msg:
-                print(msg)
-                sys.exit(1)
+            sockets_list = [sys.stdin, self.chat_socket]
+            read_ready, write_ready, except_ready = select.select(sockets_list, [], [])
+
+            for socket in read_ready:
+                if socket == self.chat_socket:
+                    try:
+                        message, address = self.chat_socket.recvfrom(Client.RECV_SIZE)
+                    except:
+                        # If no message is received, move on to next iteration of loop
+                        continue
+                    if not message:
+                        # If server has closed the connection, exit loop
+                        return
+                    print(message.decode())
+            else:
+                # Send message to chat room
+                message = sys.stdin.readline()
+                if message:
+                    self.chat_socket.sendto((self.chat_name + message).encode(), (self.chat_address, self.chat_port))
+                    # sys.stdout.write(self.chat_name + message)
+                    # sys.stdout.flush()
+
+                    # Check for exit command
+                    if message.strip() == 'exit':
+                        return
+            # try:
+            #     data, address_port = self.chat_socket.recvfrom(Client.RECV_SIZE)
+            #     address, port = address_port
+            #     print("Received: {} {}".format(data.decode('utf-8'), address_port))
+            # except KeyboardInterrupt:
+            #     print(); exit()
+            # except Exception as msg:
+            #     print(msg)
+            #     sys.exit(1)
 
 ########################################################################
 # Process command line arguments if run directly.
